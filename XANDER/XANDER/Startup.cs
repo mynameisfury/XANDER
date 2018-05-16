4930680ab1c7f647d7862c7f109b32491b4154ff
mynameisfury <jonathanweatherall0@gmail.com>
2018-05-16 08:42:53 -0500

    initial commit

---
+++ XANDER/XANDER/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XANDER.Startup))]
+namespace XANDER
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
